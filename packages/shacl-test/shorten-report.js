ac43ec3f647af035a67eb4e512caeaa7f7fd5680
Giulio V <vannini.gv@gmail.com>
2025-12-09 14:15:16 +0100

    Fix async/await in shorten-report.js for createPlaygroundUrl

--- packages/shacl-test/shorten-report.js
+++ packages/shacl-test/shorten-report.js
@@ -4,5 +4,6 @@ import { createPlaygroundUrl } from '@lindas/shacl-playground'
 
 // Note: URL shortening removed - just output the full playground URL
 ;(async function () {
-  process.stdout.write(createPlaygroundUrl(process.argv[2], process.argv[3]))
+  const url = await createPlaygroundUrl(process.argv[2], process.argv[3])
+  process.stdout.write(url)
 })()
